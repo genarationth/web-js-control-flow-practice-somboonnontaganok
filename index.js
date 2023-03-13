615ed9c45a991de6bf475d5de12d87ce839b48ff
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-13 06:45:11 +0000

    finish switch

---
+++ index.js
@@ -0,0 +1,27 @@
+let dayNumber = 0;
+dayNumber = 7;
+switch(dayNumber) {
+    case 0:
+      console.log('Today is Sunday');
+    break;
+    case 1:
+        console.log('Today is Monday');
+    break;
+    case 2:
+        console.log('Today is Tuesday');
+    break;
+    case 3:
+        console.log('Today is Wednesday');
+    break;
+    case 4:
+        console.log('Today is Thursday');
+    break;
+    case 5:
+        console.log('Today is Friday');
+    break;
+    case 6:
+        console.log('Today is Satherday');
+    break;
+    default:
+        console.log('Invalid day number please try again');
+  }
