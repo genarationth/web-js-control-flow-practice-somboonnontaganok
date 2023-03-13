0b96be735f6df7087c5297c4b78d01d94bf6594e
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-03-13 08:06:29 +0000

    compare ternaly if else and short-hand

---
+++ tool.js
@@ -0,0 +1,11 @@
+let tool = '';
+// tool = 'marker';
+// let writeUtensil = tool || 'pen';
+// if (tool) {
+//     writeUtensil = tool;
+// }
+// else {
+//     writeUtensil = 'pen';
+// }
+tool ? writeUtensil = tool : writeUtensil = 'pen';
+console.log(`The ${writeUtensil} is for you`);
